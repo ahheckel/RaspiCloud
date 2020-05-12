5e17641e033f82d4c60715ae67541ed772f70667
ahheckel <heckelandreas@googlemail.com>
2020-05-12 08:22:35 +0200

    .

--- install/template_push-to-cloud-tmp.sh
+++ install/template_push-to-cloud-tmp.sh
@@ -47,23 +47,24 @@ for ((i = 0; i < ${#syncfolders[@]}; i++)) ; do
 	if [ "$(cat $md5)" != "$(cat $_md5)" ] ; then
         nc -w 10 -z $ip 22 2>/dev/null ; if [ $? -eq 1 ] ; then echo "netcat failed. - exiting." ; rm -f $HOME/.$(basename $0).lock ; exit 1 ; fi # is more robust than ping
 		if [ $update -eq 1 ] ; then
-		  echo "---updating cloud-scripts..."     
+		  echo "---updating cloud-scripts first..."     
 		  rsync -v -c -i -e "ssh -i $ckey" ${user}@${ip}:$clidir/* $HOME/.shortcuts/ | grep ^\>f | cut -d " " -f 2- > $HOME/.dirlists/.update.scrpts
 		  if [ $(cat $HOME/.dirlists/.update.scrpts | wc -l) -gt 0 ] ; then
 		    chmod +x $HOME/.shortcuts/*.sh
-		    echo "---cloud-scripts updated - exiting..."
+		    echo "---cloud-scripts updated, syncing in next cycle - exiting..."
 		    exit 2
+		  else
+		    echo "   ...nothing to do."
 		  fi
-		fi  
-		update=0
+		  update=0
+		fi
 		echo "---deleting duplicates in $dir..."
 		fdupes -dNA "$dir"
 		echo "---syncing..."
 		rsync $opts "$dir"/* --exclude='*.*.part' --exclude='*.*.crdownload' --exclude=".*" --exclude='~*' "${md5excl[@]}" --iconv=utf-8,ascii//TRANSLIT//IGNORE -e "ssh -i $ckey" ${user}@$ip:$dstdir/.${md5n} | grep ^\<f | cut -d " " -f 2- > $HOME/.dirlists/.update.list
-		cat $HOME/.dirlists/.update.list
+		#cat $HOME/.dirlists/.update.list
 		if [ $(cat $HOME/.dirlists/.update.list | wc -l) -gt 0 ] ; then
 		  echo "---updating database..."
-		  #ssh ${user}@$ip -t $scrpt $dstdir/.${md5n}
 		  rsync -av $HOME/.dirlists/.update.list -e "ssh -i $ckey" ${user}@${ip}:$dstdir/.${md5n}/
 		  ssh -i $ckey ${user}@$ip -t $scrpt "$dstdir/.${md5n}" "$dstdir/.${md5n}/.update.list"
 		fi
